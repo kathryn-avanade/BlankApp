66704909d1e3061762f4a90dd5e0caa60ee7eccc
kathryn-avanade <kathryn.mcgregor@avanade.com>
2021-05-05 17:07:55 +0100

    practise deployment

---
+++ BlankApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlankApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
